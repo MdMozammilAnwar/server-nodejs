2c100417c7220c4acdf89e58f156d5a5c1cc948d
Anwar <anwar@uexcelerate.com>
2022-09-19 19:48:00 +0530

    heroku postgres configuration has been done

--- app/configs/db.config.js
+++ app/configs/db.config.js
@@ -22,10 +22,12 @@ module.exports = {
     max: 5,
     min: 0,
     acquire: 30000,
-    idle: 10000
+    idle: 10000 
   },
-  ssl: { 
-        require: true,
-        rejectUnauthorized: false 
-      }
+  "production": {
+    "ssl": {
+      "require": true,
+      "rejectUnauthorized": false
+    },
+  }  
 };
